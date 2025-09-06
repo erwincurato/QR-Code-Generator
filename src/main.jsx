36444f4be58a89b5ff03c0ad598fa39cc363cddb
Erwin Curato <erwincurato@gmail,com>
2025-09-06 20:20:58 +0800

    Fixing the Style import in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from "./App";
-import "./styles/index.css";
+import "./Styles/index.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
