b90aaaf51168ca95ff5952fcd804cb1ff02d755c
Erwin Curato <erwincurato@gmail,com>
2025-11-19 21:16:20 +0800

    refactor(structure): complete project restructuring with new architecture

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import QRGenerator from "../components/QRGenerator/QRGenerator";
+
+//Main QR Code Generator page
+const HomePage = () => {
+  return (
+    <div>
+      <QRGenerator />
+    </div>
+  );
+};
+
+export default HomePage;
